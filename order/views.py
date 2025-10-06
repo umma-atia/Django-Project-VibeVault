e8777f1b5b86b382dc1753e0c55af03400f0a1ed
Umma Atia <red910630@gmail.com>
2025-10-07 01:24:44 +0600

    Cart operation

--- order/views.py
+++ order/views.py
@@ -12,6 +12,7 @@ from product.models import Product
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from django.core.mail import send_mail
+from rest_framework import status
 
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
@@ -26,6 +27,18 @@ class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, Gener
             return Cart.objects.none()
         return Cart.objects.prefetch_related('items__product').filter(user=self.request.user)
 
+    def create(self, request, *args, **kwargs):
+        # Check if the user already has an active cart
+        existing_cart = Cart.objects.filter(user=request.user).first()
+
+        if existing_cart:
+            # Return the existing cart if it exists
+            serializer = self.get_serializer(existing_cart)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # Otherwise, proceed with creating a new cart
+        return super().create(request, *args, **kwargs)
+    
 
 class CartItemViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
